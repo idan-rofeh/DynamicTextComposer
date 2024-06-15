baec8a6bda6df6c9ba32138508f9b642852eca7c
“idan” <idan@custodia.ai>
2024-06-15 09:46:35 +0300

    page layout and side text

---
+++ src/app/components/side-text/side-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-text',
+  templateUrl: './side-text.component.html',
+  styleUrls: ['./side-text.component.scss']
+})
+export class SideTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
